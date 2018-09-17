5c1f27ce48a169cb02e5bbb99e595b1ff9408413
ptflp <filippov.pa@s-vfu.ru>
2018-09-18 00:43:50 +0900

    add App

---
+++ framework/base/App.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace wee/Base;
+
+/**
+ * Base class App
+ */
+class App
+{
+	private static $config;
+	public static $container;
+	public static $controller;
+	public static $session;
+
+	public static function run()
+	{
+		echo 'Run';
+	}
+}
