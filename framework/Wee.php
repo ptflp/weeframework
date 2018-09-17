1d66efbfa210fbf66502bde6d0f03a4a7209e997
ptflp <filippov.pa@s-vfu.ru>
2018-09-18 02:45:31 +0900

    restructure

---
+++ framework/Wee.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace wee;
+
+use wee/base/BaseApp;
+use php-di/php-di;
+
+/**
+ * App Class
+ */
+class Wee extends BaseWee
+{
+	public function run()
+	{
+		echo 'dir';
+	}
+}
---
+++ framework/base/BaseWee.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace wee\base;
+
+/**
+ * Base class App
+ */
+abstract class BaseWee
+{
+	private static $app;
+	private static $container;
+}
